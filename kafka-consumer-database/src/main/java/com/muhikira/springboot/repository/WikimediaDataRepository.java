c9d0cf7b0d950ea1a4e971771cfc62986941e531
Muhikira <muhikiraismael@gmail.com>
2023-08-19 23:58:31 -0500

    Initial commit

---
+++ kafka-consumer-database/src/main/java/com/muhikira/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.muhikira.springboot.repository;
+
+import com.muhikira.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
---
+++ src/main/java/com/muhikira/springbootkafkaproject/SpringbootKafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.muhikira.springbootkafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringbootKafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProjectApplication.class, args);
+	}
+
+}
